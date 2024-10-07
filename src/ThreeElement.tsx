b2f5567dda0f769ddbb923e27fe066c15605487b
n0wlk <kxwxn@icloud.com>
2024-10-07 22:47:15 +0900

    some

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -3,10 +3,13 @@ import * as THREE from "three";
 import React from "react";
 import { useRef } from "react";
 import { useControls } from "leva";
+import { Box } from "@react-three/drei";
 
 const ThreeElement = () => {
   const { size, gl, scene, camera } = useThree();
+  const groupRef = useRef<THREE.Mesh>(null);
   const boxRef = useRef<THREE.Mesh>(null);
+  const ringRef = useRef<THREE.Mesh>(null);
   const box = useControls({
     rotation: { value: 0, min: -360, max: 360, step: 1 },
   });
@@ -18,6 +21,8 @@ const ThreeElement = () => {
   // Three.js 로 mesh를 추가하는 방법
 
   useFrame((state, delta) => {
+    groupRef.current.rotation.x += 0.03;
+    scene.rotation.x += 0.01;
     console.log("boxRef:", boxRef);
     console.log("state:", state);
     console.log("delta:", delta);
@@ -26,22 +31,47 @@ const ThreeElement = () => {
     // boxRef.current.scale.z += 0.01;
   }); // useFrame은 애니메이션으로 뭔가를 만들기 위해서 필요하다.
 
+  scene.rotation.x = THREE.MathUtils.degToRad(45);
+
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
+
+      <Box position={[1, 1, 1]}>
+        <meshStandardMaterial color={"skyblue"} />
+      </Box>
+      {/* react-three/drei를 이용하여 mesh를 생성하는 방법. */}
+
       <mesh
-        ref={boxRef}
-        rotation={[
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(box.rotation),
-          0,
-        ]}
-        position={[2, 2, 2]}
-        scale={[3, 3, 3]}
-      >
-        <icosahedronGeometry />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
+        geometry={new THREE.BoxGeometry(2, 2, 2)}
+        position={[-2, -2, -2]}
+      ></mesh>
+      {/* 프로퍼티형식으로 mesh를 생성하는 방법 */}
+
+      <group position={[5, 5, 5]} ref={groupRef}>
+        <mesh position={[-3, -3, -3]}>
+          <boxGeometry />
+          <meshStandardMaterial color={"hotpink"} />
+        </mesh>
+        <mesh visible ref={ringRef} position={[7, 7, 5]}>
+          <ringGeometry args={[1, 5, 32]} />
+          <meshStandardMaterial color={"green"} side={THREE.DoubleSide} />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          rotation={[
+            THREE.MathUtils.degToRad(45),
+            THREE.MathUtils.degToRad(box.rotation),
+            0,
+          ]}
+          position={[2, 2, 2]}
+          scale={[5, 3, 3]}
+        >
+          <icosahedronGeometry />
+          <meshStandardMaterial color={"red"} />
+        </mesh>
+        {/* child형식으로 메쉬를 생성하는 방법 */}
+      </group>
     </>
   );
 };
